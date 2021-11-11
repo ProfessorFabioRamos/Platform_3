155ea941c38f7e29b13b35d809365097ee1dc00a
Fabio Ramos <fabiocasperxan@gmail.com>
2021-11-10 21:24:24 -0300

    primeiro_commit

---
+++ Platform_3/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public float direction = -1.0f;
+    public float speed = 2.0f;
+    public int enemyHP = 2;
+    private Rigidbody2D rig;
+    private SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed*direction, rig.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.layer == 9){
+            direction *= -1;
+            Flip();
+        }
+    }
+
+    void Flip(){
+        spr.flipX = !spr.flipX;
+    }
+
+    public void TakeDamage(int damage){
+        enemyHP -= damage;
+        if(enemyHP <= 0){
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Platform_3/Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header ("Movimentação")]
+    #region Movimentação
+    public float horizontalSpeed = 3.0f;
+    #endregion
+    [Header ("Pulo")]
+    #region Pulo
+    public float jumpForce = 500;
+    public LayerMask whatIsFloor;
+    #endregion
+    [Header ("Ataques")]
+    #region Ataques
+    public BoxCollider2D attackArea;
+    public int melleDamage = 1;
+    private EnemyBase enemyInArea = null;
+
+    #endregion
+    [Header ("Referencias")]
+    #region Referencias de Componentes
+    private Rigidbody2D rig;
+    private Animator anim;
+    private SpriteRenderer spr;
+    #endregion
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if(h>0) Flip(true);
+        else if(h<0)Flip(false);
+
+        anim.SetFloat("speed", Mathf.Abs(h));
+
+        rig.velocity = new Vector2(h*horizontalSpeed, rig.velocity.y);
+
+        bool grounded = Physics2D.OverlapCircle(transform.position, 0.2f, whatIsFloor);
+
+        if(grounded && Input.GetButtonDown("Jump")){
+            rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        anim.SetBool("grounded", grounded);
+
+        if(Input.GetKeyDown(KeyCode.X)){
+            anim.SetTrigger("attack");
+        }
+    }
+
+    void Flip(bool faceRight){
+        spr.flipX = !faceRight;
+    }
+
+    public void DamageEnemy(){
+        if(enemyInArea != null){
+            enemyInArea.TakeDamage(1);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other){
+        if(other.gameObject.layer == 12){
+            enemyInArea = other.GetComponent<EnemyBase>();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if(other.gameObject.layer == 12){
+            enemyInArea = null;
+        }
+    }
+}
